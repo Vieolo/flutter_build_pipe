56698a0cd68d938325fa57ebecc1c2c5710b2f08
Ramtin <Ramtin.abadi@gmail.com>
2025-07-12 15:57:25 +0200

    Added initial cache cleaning process

--- bin/build.dart
+++ bin/build.dart
@@ -17,5 +17,21 @@ void main(List<String> args) async {
   }
 
   final config = BuildConfig.fromMap(pubspec["build_pipe"]);
-  print(config);
+  if (config.platforms.isEmpty) {
+    print("No target platforms were detected. Please add your target platforms to pubspec");
+    return;
+  }
+
+  print("\nStarting the build process...\n");
+  print("The following target platforms are detected:");
+  print("${config.platforms.map((z) => z.name).join(", ")}\n");
+
+  if (config.cleanFlutter) {
+    print("Cleaning flutter existing builds...");
+    await Process.run("flutter", ["clean"]);
+    print("√ Flutter cleaned");
+    print("Getting pub packages");
+    await Process.run("flutter", ["pub", "get"]);
+    print("√ Flutter pub packages synced");
+  }
 }
--- lib/utils/config.utils.dart
+++ lib/utils/config.utils.dart
@@ -27,6 +27,7 @@ class BuildConfig {
   BuildConfigPlatform? windows;
   BuildConfigPlatform? linux;
   String? xcodeDerivedKey;
+  bool cleanFlutter;
 
   BuildConfig({
     this.android,
@@ -36,16 +37,30 @@ class BuildConfig {
     this.web,
     this.windows,
     this.xcodeDerivedKey,
+    required this.cleanFlutter,
   });
 
   factory BuildConfig.fromMap(YamlMap data) {
+    YamlMap platforms = data["platforms"] ?? {};
     return BuildConfig(
-      android: data.containsKey("android") ? BuildConfigPlatform.fromMap(data["android"], TargetPlatform.android) : null,
-      ios: data.containsKey("ios") ? BuildConfigPlatform.fromMap(data["ios"], TargetPlatform.ios) : null,
-      macos: data.containsKey("macos") ? BuildConfigPlatform.fromMap(data["macos"], TargetPlatform.macos) : null,
-      linux: data.containsKey("linux") ? BuildConfigPlatform.fromMap(data["linux"], TargetPlatform.linux) : null,
-      windows: data.containsKey("windows") ? BuildConfigPlatform.fromMap(data["windows"], TargetPlatform.windows) : null,
-      web: data.containsKey("web") ? BuildConfigPlatform.fromMap(data["web"], TargetPlatform.web) : null,
+      android: platforms.containsKey("android") ? BuildConfigPlatform.fromMap(platforms["android"], TargetPlatform.android) : null,
+      ios: platforms.containsKey("ios") ? BuildConfigPlatform.fromMap(platforms["ios"], TargetPlatform.ios) : null,
+      macos: platforms.containsKey("macos") ? BuildConfigPlatform.fromMap(platforms["macos"], TargetPlatform.macos) : null,
+      linux: platforms.containsKey("linux") ? BuildConfigPlatform.fromMap(platforms["linux"], TargetPlatform.linux) : null,
+      windows: platforms.containsKey("windows") ? BuildConfigPlatform.fromMap(platforms["windows"], TargetPlatform.windows) : null,
+      web: platforms.containsKey("web") ? BuildConfigPlatform.fromMap(platforms["web"], TargetPlatform.web) : null,
+      xcodeDerivedKey: data["xcodeDerivedDataPathEnvKey"],
+      cleanFlutter: data["clean_flutter"] ?? true,
     );
   }
+
+  bool get needXCodeDerivedCleaning => (ios != null || macos != null) && xcodeDerivedKey != null && xcodeDerivedKey!.isNotEmpty;
+  List<TargetPlatform> get platforms => [
+    if (ios != null) TargetPlatform.ios,
+    if (android != null) TargetPlatform.android,
+    if (macos != null) TargetPlatform.macos,
+    if (linux != null) TargetPlatform.linux,
+    if (windows != null) TargetPlatform.windows,
+    if (web != null) TargetPlatform.web,
+  ];
 }
