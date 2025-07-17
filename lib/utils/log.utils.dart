6f04f162eb4943a63efd1687c12b6cc3ad22b06a
Ramtin <Ramtin.abadi@gmail.com>
2025-07-17 18:52:59 +0200

    Centralized the appending to the log file

--- lib/utils/log.utils.dart
+++ lib/utils/log.utils.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:build_pipe/utils/config.utils.dart';
+
 class LogUtils {
   static List<String> getActionStartLines(String action) {
     return [
@@ -13,5 +17,25 @@ class LogUtils {
       "\n-- detail end -----------------------",
       "** action end **********************\n\n",
     ];
+  }
+
+  static Future<void> appendLogUsingStringList(BuildConfig config, List<String> logLines) async {
+    if (!config.generateLog) return;
+
+    File logFile = File(config.logFile);
+    IOSink? logSink;
+    bool alreadyExists = true;
+
+    if (!(await logFile.exists())) {
+      logFile = await File(config.logFile).create(recursive: true);
+      alreadyExists = false;
+    }
+
+    logSink = logFile.openWrite(mode: alreadyExists ? FileMode.append : FileMode.write);
+
+    for (var line in logLines) {
+      logSink.writeln(line);
+    }
+    await logSink.close();
   }
 }
