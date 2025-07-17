ec84aaaa0b94c60fd0bf75ec8db2b32158955476
Ramtin <Ramtin.abadi@gmail.com>
2025-07-17 18:52:46 +0200

    Added support for clearing XCode derived data

--- bin/build.dart
+++ bin/build.dart
@@ -2,31 +2,45 @@ import 'dart:io';
 
 import 'package:build_pipe/utils/builder.utils.dart';
 import 'package:build_pipe/utils/config.utils.dart';
+import 'package:build_pipe/utils/console.utils.dart';
 import 'package:build_pipe/utils/process.utils.dart';
+import 'package:build_pipe/utils/xcode.utils.dart';
 import 'package:yaml/yaml.dart' as yaml;
 
 void main(List<String> args) async {
   final rawPubspecFile = File('pubspec.yaml');
   if (!(await rawPubspecFile.exists())) {
-    print("pubspec.yaml file could not be found!");
+    Console.logError("pubspec.yaml file could not be found!");
     return;
   }
 
   final pubspec = yaml.loadYaml(await rawPubspecFile.readAsString());
   if (!(pubspec as yaml.YamlMap).containsKey("build_pipe")) {
-    print("please add the build_pipe configuration to your pubspec file!");
+    Console.logError("please add the build_pipe configuration to your pubspec file!");
     return;
   }
 
   final config = BuildConfig.fromMap(pubspec["build_pipe"], pubspec["version"].split("+")[0]);
   if (config.platforms.isEmpty) {
-    print("No target platforms were detected. Please add your target platforms to pubspec");
+    Console.logError("No target platforms were detected. Please add your target platforms to pubspec");
     return;
   }
 
-  print("\nStarting the build process...\n");
+  Console.logInfo("\nStarting the build process...\n");
   print("The following target platforms are detected:");
   print("${config.platforms.map((z) => z.name).join(", ")}\n");
+
+  if (config.xcodeDerivedKey != null && config.xcodeDerivedKey!.isNotEmpty && config.platforms.any((z) => z == TargetPlatform.ios || z == TargetPlatform.macos)) {
+    String? xcodePath = Platform.environment[config.xcodeDerivedKey!];
+    if (xcodePath == null || xcodePath.isEmpty) {
+      Console.logError("${config.xcodeDerivedKey} is either not a valid environmental variable or has an empty value!");
+    } else {
+      bool deleted = await XCodeUtils.deleteDerivedData(config, xcodePath);
+      if (!deleted) {
+        exit(1);
+      }
+    }
+  }
 
   if (config.cleanFlutter) {
     await ProcessHelper.runCommandUsingConfig(
