108a72a9f4e96877bcaa4817cdbd2662a2fefc23
Ramtin <Ramtin.abadi@gmail.com>
2026-01-15 17:59:22 +0100

    Improved the terminal log of the publish command

--- bin/publish.dart
+++ bin/publish.dart
@@ -1,10 +1,28 @@
+import 'dart:io';
+
 import 'package:build_pipe/config/config.dart';
+import 'package:build_pipe/utils/console.utils.dart';
 
 /// https://help.apple.com/asc/appsaltool/#/apdATD1E53-D1E1A1303-D1E53A1126
 /// Main entry point of the `dart run build_pipe:publish` command
 void main(List<String> args) async {
   // Reading the config
   BPConfig config = await BPConfig.readPubspec(args);
+
+  if (config.publishPlatforms.isEmpty) {
+    Console.logError("No target platforms were detected for publish. Please add your target platforms to pubspec");
+    exit(1);
+  }
+
+  Console.logInfo("\nStarting the publish process...\n");
+  print("The following target platforms are detected:");
+  for (var i = 0; i < config.publishPlatforms.length; i++) {
+    String prefix = "├──";
+    if (i == config.publishPlatforms.length - 1) {
+      prefix = "└──";
+    }
+    print("$prefix ${config.publishPlatforms[i].name}${i == config.publishPlatforms.length - 1 ? "\n" : ""}");
+  }
 
   // -=-=-=-=-=-=
   // iOS
